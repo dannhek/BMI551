6d5f7e8fe0f14a39b5794755c2e37b6701e184cb
Dann Hekman <dhek@Danns-MacBook-Air.local>
2019-10-19 22:06:17 -0500

    Update DF Name

--- BMI551_Homework_3_support.r
+++ BMI551_Homework_3_support.r
@@ -2,7 +2,7 @@ library(Hmisc)
 library(knitr)
 
 probability_range <- seq(0.100,0.700,0.001) 
-upper_bound <- data.frame(
+possible_bounds <- data.frame(
       P = probability_range
       ,`P(k=0)` = round(dbinom(0,20,probability_range),6)
       ,`P(k=1)` = round(dbinom(1,20,probability_range),6)
@@ -11,5 +11,5 @@ upper_bound <- data.frame(
       ,`P(k=4)` = round(dbinom(4,20,probability_range),6)
       ,`P(k=5)` = round(dbinom(5,20,probability_range),6)
 ) 
-names(upper_bound)<-c('proposed_pi','P(k=0)','P(k=1)','P(k=2)','P(k=3)','P(k=4)','P(k=5)')
-kable(upper_bound)
+names(possible_bounds)<-c('possible_pi','P(k=0)','P(k=1)','P(k=2)','P(k=3)','P(k=4)','P(k=5)')
+kable(possible_bounds)
